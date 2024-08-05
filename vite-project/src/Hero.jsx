8fb70b3813b98fac382267be3dee155514b65cc0
Durbek08 <durbekacer@gmail.com>
2024-08-05 09:46:47 +0500

    Salom

--- vite-project/src/Hero.jsx
+++ vite-project/src/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
             <div className="">
                 <h1 className=" font-bold text-4xl md:leading-[70px] md:text-left  md:text-[64px] text-center max-w-[600px] mb-6 text-[#DEDEDE]">Hi, I am Your Name A Product Designer based in City.</h1>
                 <p className="font-semibold text-[#959595] max-w-[500px] mb-[64px] text-2xl">I help businesses and companies reach  their goals by designing user-centric digital products & interactive experiences.</p>
-                <button className="flex gap-4 items-center  lg:mr-auto py-5 bg-[#9D9D9D] px-[101px] rounded-md font-semibold text-lg text-[#FFFFFF]">   
+                <button className="flex gap-4 items-center lg:mr-auto py-5 bg-[#9D9D9D] px-[101px] rounded-md font-semibold text-lg text-[#FFFFFF]">   
                     <svg width="23" height="18" viewBox="0 0 23 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M20.6429 1H2.35715C1.72597 1 1.21429 1.51167 1.21429 2.14286V15.8571C1.21429 16.4883 1.72597 17 2.35715 17H20.6429C21.274 17 21.7857 16.4883 21.7857 15.8571V2.14286C21.7857 1.51167 21.274 1 20.6429 1Z" stroke="white" stroke-width="2" stroke-linecap="round"/>
                     <path d="M1.21429 2.71436L11.5 9.00007L21.7857 2.71436" stroke="white" stroke-width="2" stroke-linecap="round"/>
