970b16bb84349cc977888e1199c973cac3185be7
Durbek08 <durbekacer@gmail.com>
2024-08-14 10:56:11 +0500

    Salom

---
+++ Visual.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Visual() {
+  return (
+    <div className="">
+        <div className="">
+            <h4 className="">Visual Explorations</h4>
+            <div className="">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Hero from './Hero'
 import Featured from './Featured'
+import Visual from './Visual'
 
 
 
@@ -11,7 +12,7 @@ export default function  App() {
       <Header/>
       <Hero/>
       <Featured/>
-      
+      <Visual/>
     </div>
   )
 }
